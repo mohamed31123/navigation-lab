631911f489d112c6709f3be8a1f7c679a2e5aebd
mohamed <m.eddinari5197@uca.ac.ma>
2026-03-21 17:00:27 +0100

    the first commit

---
+++ src/Accueil.js
@@ -0,0 +1,17 @@
+import ListeTaches from './ListeTaches';
+
+import Logo from './logo';
+import LecteurAudio from './LecteurAudio';
+
+function Accueil() {
+  return (
+    <div>
+      <h2>Page d’accueil</h2>
+      <Logo />
+      <LecteurAudio />
+      <ListeTaches />
+    </div>
+  );
+}
+
+export default Accueil;
---
+++ src/Logo.js
@@ -0,0 +1,7 @@
+import logo from './logo.png';
+
+function Logo() {
+  return <img src={logo} alt="Logo du site" width="150" />;
+}
+
+export default Logo;
